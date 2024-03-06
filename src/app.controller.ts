2f3e2a993de9e7a178c09c9b2b0d1deef8eddf47
root <root@DESKTOP-JGMP2PO>
2024-03-05 21:54:32 -0500

    [ADD]Initial params and routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('nuevo')
+  myMethod() {
+    return 'New Route';
+  }
+  /*normal way */
+  // @Get('product/:id')
+  // getProduct(@Param() params: any) {
+  //   return `Product ${params.id}`;
+  // }
+
+  @Get('product/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `Product ${productId}`;
+  }
+
+  @Get('categories/:categorieId')
+  getCategorie(@Param('categorieId') categorieId: string) {
+    return `Ctegorie ${categorieId}`;
+  }
+
+  @Get('categories/:categorieId/products/:productId')
+  getCategorieProduct(
+    @Param('categorieId') categorieId: string,
+    @Param('productId') productId: string,
+  ) {
+    return `Product ${productId}Categorie ${categorieId}`;
   }
 }
